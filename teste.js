65a5d955e43191b4ec661bf4381e03be12c8b203
Nadirlene <nadirleneoliveira@yahoo.com>
2021-06-01 07:14:01 -0300

    testes.js

--- teste.js
+++ teste.js
@@ -1,5 +1,21 @@
-var lista = [0, 1 , 2, 3, 4];
+// var lista = [0, 1 ,6, 5,1,0,3,2, 2, 3, 4];
 
 
+// const hash = {}
+// for( valor of lista){
+//     hash[valor] = valor
+    
+// }
 
-console.log(lista.length)
+// resultado = []
+// console.log(resultado)
+// hash.forEach( valor =>  {console.log(hash.valor)
+   
+// })
+//console.log(resultado)
+const array = ["André", 1, 2, 3, "André", 3, "Pedro"];
+
+array.reduce((unico, item) => {
+    return unico.includes(item) ? unico : [...unico, item]
+}, []);
+
